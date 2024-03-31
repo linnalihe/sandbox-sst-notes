4641c902714ce1bf1d73a60aa85aee9c90d25737
Linna Li <linnali577@gmail.com>
2024-03-31 17:43:22 -0400

    add billing api

---
+++ packages/functions/src/billing.ts
@@ -0,0 +1,29 @@
+import Stripe from "stripe";
+import { Config } from "sst/node/config";
+import handler from "@sandbox-sst-notes/core/handler";
+import { calculateCost} from "@sandbox-sst-notes/core/cost";
+
+export const main = handler(async (event) =>{
+    console.log("entering billing.main")
+    console.log("event", event)
+
+    const { storage, source } = JSON.parse(event.body || "{}");
+    const amount = calculateCost(storage)
+    const description = "Scratch charge"
+
+    
+
+    const stripe = new Stripe(Config.STRIPE_SECRET_KEY, {
+        apiVersion: "2023-10-16"
+    })
+
+    await stripe.charges.create({
+        source,
+        amount,
+        description,
+        currency: "usd",
+    });
+
+    return JSON.stringify({status: true})
+
+})
